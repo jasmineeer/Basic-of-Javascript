89c4c9fe624a1c350c0eab8dcf6e1559fe6c6d52
jasmine aqilah <jasmine_aqilah_29rpl@student.smktelkom-mlg.sch.id>
2022-01-19 07:30:57 +0700

    Basic of Javascript Assignment

--- assignment3.js
+++ assignment3.js
@@ -1,4 +1,4 @@
-let nama = "Rhodey", bb = 90, tb = 170
+let nama = "Rhodey", bb = 90, tb = 1.7
 
     let bmi = bb / (tb*tb)
     if (bmi < 18.5) {
@@ -7,7 +7,7 @@ let nama = "Rhodey", bb = 90, tb = 170
     if (bmi >= 18.5 && bmi <= 24.9) {
         console.log("Status Berat Badan = Normal")
     }
-    if (bmi > 25 && bmi <= 29.9) {
+    if (bmi >= 25 && bmi <= 29.9) {
         console.log("Status Berat Badan = Kelebihan")
     }
     if (bmi >= 30) {
